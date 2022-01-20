79aacf20262a0bc0460795e4466737e7f0d57ded
naveedwani <waninaveed01@gmail.com>
2022-01-20 15:35:37 +0530

    Create maven.groovy

---
+++ vars/maven.groovy
@@ -0,0 +1,40 @@
+    def call(String repoUrl) {
+
+           stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           } 
+         
+         stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+
+
+        stage('Stability') {
+          steps {
+           sh '''mvn compile'''
+        // sh '''mvn pmd:pmd'''
+          }
+          }
+          
+          stage('Quality') {
+          steps {
+           sh '''mvn checkstyle:checkstyle'''
+         
+          }
+          }
+                     
+          stage('Code Coverage') {
+          steps {
+            sh '''mvn cobertura:cobertura'''
+         
+          }
+          }
+             
+        }   
